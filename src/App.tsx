0c6e5db5dfa93eb96d1bf85e2c697584f77e468b
david <david.nesterovic@hotmail.com>
2026-01-03 11:10:42 +0100

    yur

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
       <>
           <div className="text-3xl underline font-bold ">
-                Hello World!
+                Forntnite
           </div>
       </>
   )
