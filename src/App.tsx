1ecccaed61950e7b8540279fe0899430040b4816
david <david.nesterovic@hotmail.com>
2026-01-03 13:40:02 +0100

    lost and found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
+import Found from "./pages/Found.tsx";
+import Lost from "./pages/Lost.tsx";
 import MainLayout from './layouts/MainLayout'
 
 const App = () => {
@@ -8,6 +10,8 @@ const App = () => {
         <Routes>
             <Route element={<MainLayout />}>
                 <Route path="/" element={<Home />} />
+                <Route path="/lost" element={<Lost />} />
+                <Route path="/found" element={<Found />} />
             </Route>
 
             <Route path="*" element={<NotFound />} />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
     return (
-        <div className="p-8">
+        <div>
             <h1 className="text-2xl font-bold">Home</h1>
             <p className="mt-2 text-gray-600">
                 Fortnite
