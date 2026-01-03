a45421fed6ea119b99793ed3acafe30e40a47f7e
david <david.nesterovic@hotmail.com>
2026-01-03 10:45:19 +0100

    Erster Fortineit commitment

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+function App() {
+
+  return (
+      <>
+          <div className="text-3xl underline font-bold ">
+              Fortnite
+          </div>
+      </>
+  )
+}
+
+export default App
